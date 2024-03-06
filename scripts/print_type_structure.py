2973ad54e298fb10d8a38ffd3f9a6b6e4fd9bfad
rajeshrah22 <rajeshrah22@gmail.com>
2024-03-05 23:09:47 -0500

    Changed it to print tree like strucutre representation of JSON document.

--- scripts/print_type_structure.py
+++ scripts/print_type_structure.py
@@ -1,18 +1,17 @@
 import json
 import os
 
-def print_type_structure(data, indent=0, file=None):
-    for key, value in data.items():
+def print_type_structure(data, indent=0, is_last=True, file=None):
+    for i, (key, value) in enumerate(data.items()):
+        is_last = i == len(data) - 1
         if isinstance(value, dict):
-            print(f'interface {key.capitalize()} {{', file=file)
-            print_type_structure(value, indent + 1, file=file)
-            print('}', file=file)
+            print('  ' * indent + (f'|-- {key}: object' if is_last else f'|-- {key}: object'), file=file)
+            print_type_structure(value, indent + 1, is_last, file=file)
         elif isinstance(value, list) and value and isinstance(value[0], dict):
-            print(f'interface {key.capitalize()}[] {{', file=file)
-            print_type_structure(value[0], indent + 1, file=file)
-            print('}', file=file)
+            print('  ' * indent + (f'|-- {key}: array' if is_last else f'|-- {key}: array'), file=file)
+            print_type_structure(value[0], indent + 1, is_last, file=file)
         else:
-            print(f'  {key}: {type(value).__name__};', file=file)
+            print('  ' * indent + (f'|-- {key}: {type(value).__name__}' if is_last else f'|-- {key}: {type(value).__name__}'), file=file)
 
 # Use os.path.join to construct the file paths
 input_file_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'output.json'))
