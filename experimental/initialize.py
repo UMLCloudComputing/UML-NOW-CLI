946dc9079d339c1ba28606fae739aacb5dc41bb9
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-08-06 23:28:50 -0400

    chore!: deleted chromadb binaries.

--- experimental/initialize.py
+++ experimental/initialize.py
@@ -3,8 +3,19 @@ from sentence_transformers import SentenceTransformer
 from chromadb.utils import embedding_functions
 import argparse
 import requests
+import os
+from dotenv import *
+load_dotenv()
+
+# openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+#     api_key=os.getenv('OPENAI_API_KEY'),
+#     model_name="text-embedding-3-small"
+# )
 
 default_ef = embedding_functions.DefaultEmbeddingFunction()
+
+# Initialize ChromaDB client
+client = chromadb.PersistentClient(path="ChromaDB")
 
 def embed_text(client, url):
     collection = client.create_collection(name="my_collection", embedding_function=default_ef)
@@ -34,3 +45,5 @@ def embed_text(client, url):
         documents=titles,
         ids=ids
     )
+
+embed_text(client, "https://www.uml.edu/api/registrar/course_catalog/v1.0/courses?field=subject&query=COMP")
